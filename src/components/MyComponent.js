a2b68f78dc2fc5edf4f18553726290438d41ba49
Minhnhat21 <nguyenhuuminhnhatasus@gmail.com>
2023-01-30 20:32:18 +0700

    video-16

---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import { render } from "@testing-library/react";
+import React from "react";
+
+class MyComponent extends React.Component{
+    render() {
+        return(
+            <div>Hello, This is my component</div>
+        )
+    }
+}
+
+export default MyComponent;
