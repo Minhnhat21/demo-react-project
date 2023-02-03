b0e7dd2cdb25f1577bb76fdef578cbf3dd1c7d3c
Minhnhat21 <nguyenhuuminhnhatasus@gmail.com>
2023-02-03 19:45:02 +0700

    video-22

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,54 +1,13 @@
 import { render } from "@testing-library/react";
 import React from "react";
+import UserInfo from "./UserInfor";
 
 class MyComponent extends React.Component{
-    state = {
-        name: 'Minh Nhat',
-        address: 'Thành Phố Huế',
-        age: 21
-    }
-    handleClick(event) {
-        console.log('Xin chao');
-        
-        //merge state => react class
-        this.setState({
-            name: 'Minh Quang',
-            age: Math.floor((Math.random() * 100) + 1)
-        })
-
-        // this.setState({
-        //     age: Math.floor((Math.random() * 100) + 1)
-        // })
-    }
-
-    handleOnMouseOver(event) {
-        // console.log(event.clientX);
-    }
-
-    handleOnChangeInput = (event) => {
-        this.setState({
-            name: event.target.value
-        })
-    }
-
-    handleOnSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state.name)
-        // this.setState({
-        //     name: event.target.value
-        // })
-    }
+    
     render() {
         return(
             <div>
-                Hello, I'm {this.state.name}, {this.state.age} year old. 
-                <form onSubmit={(event) => {this.handleOnSubmit(event)}}>
-                    <input
-                        type = 'text'
-                        onChange={(event) => {this.handleOnChangeInput(event)}}
-                    />
-                    <button>Sunmit</button>
-                </form>
+               <UserInfo/>
             </div>
         )
     }
