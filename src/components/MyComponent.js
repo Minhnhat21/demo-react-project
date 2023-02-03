21a11ac103e0c177b0cbd6fc74fe968f34b8b40c
Minhnhat21 <nguyenhuuminhnhatasus@gmail.com>
2023-02-03 10:10:04 +0700

    video-20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -7,18 +7,28 @@ class MyComponent extends React.Component{
         address: 'Thành Phố Huế',
         age: 21
     }
-    handleClick() {
+    handleClick(event) {
         console.log('Xin chao');
+        
+        //merge state => react class
+        this.setState({
+            name: 'Minh Quang',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+
+        // this.setState({
+        //     age: Math.floor((Math.random() * 100) + 1)
+        // })
     }
 
     handleOnMouseOver(event) {
-        console.log(event);
+        // console.log(event.clientX);
     }
     render() {
         return(
             <div>
-                Hello, I'm{this.state.name}, {this.state.age} year old. 
-                <button onClick={this.handleClick}>Click me</button>
+                Hello, I'm {this.state.name}, {this.state.age} year old. 
+                <button onClick={(event) => {this.handleClick(event)}}>Click me</button>
                 <button onMouseOver={this.handleOnMouseOver}>OnMouseOver</button>
             </div>
         )
