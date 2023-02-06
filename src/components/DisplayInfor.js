a14cdf1c4adcce26c6e2e59e677409d3c7b30642
Minhnhat21 <nguyenhuuminhnhatasus@gmail.com>
2023-02-06 15:22:52 +0700

    video-25: condition output

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,19 +1,40 @@
 import React from "react";
 
 class DisplayInfor extends React.Component {
+    
+    state = {
+        isShowListUser: true
+    }
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser: !this.state.isShowListUser
+        })
+    }
     render() {
 
         //destructing array/object
         const { listUsers } = this.props;  // const listUsers = this.props.listUsers
         console.log(listUsers);
+
         return(
             <div>
-                {
-                    listUsers.map((user) => {
-                        return (
-                            <div key={user.id}>My name is {user.name}, I'm {user.age} year old.</div>
-                        )
-                    })
+                <div>
+                    <span onClick={() => this.handleShowHide()}>
+                        {this.state.isShowListUser ? "Hide " : "Show "} list users: 
+                    </span>
+                </div>
+                {this.state.isShowListUser && 
+                    <div>
+                        {
+                            listUsers.map((user) => {
+                                return (               
+                                    <div key={user.id} className={user.age >= 18 ? 'green': 'red'}>
+                                        My name is {user.name}, I'm {user.age} year old.
+                                    </div>
+                                )
+                            })
+                        }
+                    </div>
                 }
             </div>
         )
