69896cf1794477348af0df1e00f8a4203cb73670
Minhnhat21 <nguyenhuuminhnhatasus@gmail.com>
2023-02-06 14:43:00 +0700

    video-24: props list object, key unique

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,9 +2,19 @@ import React from "react";
 
 class DisplayInfor extends React.Component {
     render() {
+
+        //destructing array/object
+        const { listUsers } = this.props;  // const listUsers = this.props.listUsers
+        console.log(listUsers);
         return(
             <div>
-                My name is {this.props.name}, I'm {this.props.age} year old. {this.props.address}
+                {
+                    listUsers.map((user) => {
+                        return (
+                            <div key={user.id}>My name is {user.name}, I'm {user.age} year old.</div>
+                        )
+                    })
+                }
             </div>
         )
     }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -5,14 +5,21 @@ import UserInfo from "./UserInfor";
 
 class MyComponent extends React.Component{
     
+    state = {
+        listUsers: [
+            {id: 1, name: 'Minh Nhat', age: 21},
+            {id: 2, name: 'Minh Quang', age: 14},
+            {id: 3, name: 'Quang Teo', age: 35}
+        ]
+    }
+
     render() {
         const myAddress = ['Hue','Viet Nam'];
         return(
             <div>
                <UserInfo/>
                <br/>
-               <DisplayInfor name='Nhat' age={21}/>
-               <DisplayInfor name='Quang' age={14} address = {myAddress}/>
+               <DisplayInfor listUsers = {this.state.listUsers} />
             </div>
         )
     }
